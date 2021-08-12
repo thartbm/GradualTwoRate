10cc41c59c5a384b6a0c498967442b546cf9e5ce
thartbm <thartbm@gmail.com>
2021-08-11 22:00:47 -0400

    finished functions for running ANOVAs and the main Bayes Factor analyses on all data

--- R/statistics.R
+++ R/statistics.R
@@ -1,239 +1,190 @@
 
-# DATA HANDLING -----
+require('afex')
+require('BayesFactor')
 
-# this returns parsed data
-# using the parse function specified
-# for every group and participant present
-# 
-# data can be a (nested) list with data frames
-# or a data frame (parseData is recursive)
-#
-# functions defined here:
-# - block (get average reach deviations in a few specified blocks)
-# - baseline (subtract average reach deviations from the second half of the aligned phase, per target)
-# - addtrial (adds a column in the data frame with trial numbers)
-# - addphase (adds a column in the data frame with phase numbers)
+# ANOVAs -----
 
-parseData <- function(data, FUN) {
+doExpANOVAs <- function(exp=NULL, groups=NULL) {
   
-  
-  # data has to either be a list of data frames
-  # or a data frame
-  if (is.data.frame(data)) {
-    # if it is a data frame, we process it
-    return(FUN(data))
+  if (is.null(groups)) {
+    # we need to know the groups in the experiment:
+    info <- getInfo()
+    groups <- info$group[which(info$experiment == exp)]
   }
   
-  if (is.list(data)) {
-    # if it is a list, we loop through it's entries:
+  # we get the data form these groups:
+  data <- getSelectedGroupsData(groups=groups)
+  
+  # we pre-process it, so that it is ready for ANOVAs
+  data <- parseData(data, FUN=baseline)
+  data <- parseData(data, FUN=addtrial)
+  data <- parseData(data, FUN=normalize)
+  data <- parseData(data, FUN=block)
+  
+  # we collect all data here for an ANOVA on all the data:
+  df <- NA
+  
+  group_anovas <- list()
+  
+  for (group in names(data)) {
     
-    for (itemno in c(1:length(data))) {
+    # we collect group data here, so we can do group-based ANOVAs:
+    g_df <- NA
+    
+    for (condition in c('abrupt', 'gradual')) {
       
-      data[[itemno]] <- parseData(data=data[[itemno]], FUN=FUN)
+      # add group and condition variables to the data frame:
+      gc_df <- data[[group]][[condition]]
+      gc_df$group <- group
+      gc_df$condition <- condition
+      
+      # collect data in group data frame:
+      if (is.data.frame(g_df)) {
+        g_df <- rbind(g_df, gc_df)
+      } else {
+        g_df <- gc_df
+      }
       
     }
     
-    return(data)
-  }
-  
-  # this should never happen:
-  print('can not parse this data, returning unchanged')
-  return(data)
-  
-}
-
-block <- function(df) {
-  
-  # we want to loop through participants:
-  participants <- unique(df$participant)
-  
-  # we collect blocked data here:
-  blocked <- NA
-  
-  # what are the blocks?
-  ntrials <- length(which(df$participant == participants[1]))
-  
-  if (ntrials == 164) {
-    rotated_idx <- c(123:132) # last ten?
-    counter_idx <- c(141:144) # last four?
-    clamped_idx <- c(155:164) # last ten?
-  }
-  if (ntrials == 220) {
-    rotated_idx <- c(151:160) # last ten?
-    counter_idx <- c(177:180) # last four?
-    clamped_idx <- c(211:220) # last ten?
-  }
-  
-  # loop through participants
-  for (participant in participants) {
+    # make factors:
+    for (column in c('participant','condition','group')) {
+      g_df[,column] <- as.factor(g_df[,column])
+    }
     
-    # get the participants data
-    pp_idx <- which(df$participant == participant)
-    pp_df <- df[pp_idx,]
+    # do group ANOVA on group data:
     
-    # get their blocked data...
-    rotated <- mean(pp_df$reachdeviation_deg[rotated_idx], na.rm=TRUE)
-    counter <- mean(pp_df$reachdeviation_deg[counter_idx], na.rm=TRUE)
-    clamped <- mean(pp_df$reachdeviation_deg[clamped_idx], na.rm=TRUE)
+    g_aov <- afex::aov_ez(id = "participant",
+                          dv = "reachdeviation_deg",
+                          data = g_df,
+                          within = c("block","condition"),
+                          #observed = 'reachdeviation_deg',
+                          type = 3,
+                          )
     
-    pp_block <- data.frame( block=c('rotated', 'counter', 'clamped'),
-                            reachdeviation_deg=c(rotated, counter, clamped))
-    pp_block$participant <- participant
+    group_anovas[[group]] <- g_aov
     
-    if (is.data.frame(blocked)) {
-      blocked <- rbind(blocked, pp_block)
+    
+    # add group data to all data:
+    
+    if (is.data.frame(df)) {
+      df <- rbind(df,g_df)
     } else {
-      blocked <- pp_block
+      df <- g_df
     }
     
   }
   
-  return(blocked)
+  # # make factors:
+  # for (column in c('participant','condition','group')) {
+  #   df[,column] <- as.factor(df[,column])
+  # }
   
-}
-
-
-baseline <- function(df) {
-  
-  # we want to loop through participants and targets:
-  participants <- unique(df$participant)
-  
-  # loop through participants:
-  for (participant in participants) {
+  # do ANOVA on all groups:
+  if (!is.null(exp)) {
+    e_aov <- afex::aov_ez(id = "participant",
+                          dv = "reachdeviation_deg",
+                          data = df,
+                          within = c("block","condition"),
+                          between = "group",
+                          type = 3 )
     
-    # get the participants' data
-    pp_idx <- which(df$participant == participant) 
-    pp_df <- df[pp_idx,]
-    
-    # targets for the participant:
-    targets <- unique(pp_df$targetangle_deg)
-    
-    if (dim(pp_df)[1] == 164) {
-      baseline_idx <- c(17:32)
+    cat(sprintf('\n===== Experiment: %d =====\n',exp ))
+    print(summary(e_aov))
+  } else {
+    # print the group-based ANOVAs:
+    for (group in names(group_anovas)) {
+      cat(sprintf('\n===== %s =====\n',toupper(group) ))
+      print(summary(group_anovas[[group]]))
     }
-    if (dim(pp_df)[1] == 220) {
-      baseline_idx <- c(21:40)
-    }
-    
-    for (target in targets) {
-      
-      # which trials correspond to this target
-      target_idx <- which(pp_df$targetangle_deg == target)
-      
-      # this is the average reach deviation for the target during aligned phase:
-      baseline <- mean( pp_df$reachdeviation_deg[ intersect(target_idx, baseline_idx) ], na.rm=TRUE )
-      
-      # subtract from all reaches to the target:
-      pp_df$reachdeviation_deg[target_idx] <- pp_df$reachdeviation_deg[target_idx] - baseline
-      
-    }
-    
-    # put back in main data frame
-    df$reachdeviation_deg[pp_idx] <- pp_df$reachdeviation_deg
-    
   }
   
-  # return baselined data frame:
-  return(df)
-  
 }
 
-addtrial <- function(df) {
+# Bayesian stats -----
+
+doConDiffBF <- function(exp=NULL, groups=NULL) {
   
-  # we want to loop through participants and targets:
-  participants <- unique(df$participant)
+  if (is.null(groups)) {
+    # we need to know the groups in the experiment:
+    info <- getInfo()
+    groups <- info$group[which(info$experiment == exp)]
+  }
   
-  output <- NA
+  # we get the data form these groups:
+  data <- getSelectedGroupsData(groups=groups)  
   
-  # loop through participants:
-  for (participant in participants) {
+  # we pre-process it, so that it is ready for BF stats
+  data <- parseData(data, FUN=baseline)
+  data <- parseData(data, FUN=addtrial)
+  data <- parseData(data, FUN=normalize)
+  data <- parseData(data, FUN=block)
+  
+  # we collect all data here for an ANOVA on all the data:
+  df <- NA
+  
+  # ttestBF {BayesFactor}
+  
+  group_BFs <- list()
+  
+  for (group in names(data)) {
     
-    # get the participants' data
-    pp_idx <- which(df$participant == participant) 
-    pp_df <- df[pp_idx,]
+    # we collect group data here, so we can do group-based ANOVAs:
+    g_df <- NA
     
-    pp_df$trial <- c(1:dim(pp_df)[1])
+    group_BFs[[group]] <- list()
     
-    if (is.data.frame(output)) {
-      output <- rbind(output, pp_df)
+    for (condition in c('abrupt', 'gradual')) {
+      
+      # add group and condition variables to the data frame:
+      gc_df <- data[[group]][[condition]]
+      gc_df$group <- group
+      gc_df$condition <- condition
+      
+      # collect data in group data frame:
+      if (is.data.frame(g_df)) {
+        g_df <- rbind(g_df, gc_df)
+      } else {
+        g_df <- gc_df
+      }
+      
+    }
+    
+    # do Bayes Factors for all group
+    
+    x = g_df$reachdeviation_deg[which(g_df$condition == 'abrupt' & g_df$block == 'rotated')]
+    y = g_df$reachdeviation_deg[which(g_df$condition == 'gradual' & g_df$block == 'rotated')]
+    group_BFs[[group]][['rotated']] <- BayesFactor::ttestBF(x=x, y=y, paired=TRUE)
+    x = g_df$reachdeviation_deg[which(g_df$condition == 'abrupt' & g_df$block == 'clamped')]
+    y = g_df$reachdeviation_deg[which(g_df$condition == 'gradual' & g_df$block == 'clamped')]
+    group_BFs[[group]][['clamped']] <- BayesFactor::ttestBF(x=x, y=y, paired=TRUE)
+    
+    # add group data to all data:
+    
+    if (is.data.frame(df)) {
+      df <- rbind(df,g_df)
     } else {
-      output <- pp_df
-    }
-
-  }
-  
-  # return data frame with trial numbers:
-  return(output)
-  
-}
-
-
-addphase <- function(df) {
-  
-  # we want to loop through participants and targets:
-  participants <- unique(df$participant)
-  
-  output <- NA
-  
-  # loop through participants:
-  for (participant in participants) {
-    
-    # get the participants' data
-    pp_idx <- which(df$participant == participant) 
-    pp_df <- df[pp_idx,]
-    
-    if (dim(pp_df)[1] == 164) {
-      pp_df$phase <- c(rep(1,32), rep(2,100), rep(3,12), rep(4,20))
-    }
-    if (dim(pp_df)[1] == 220) {
-      pp_df$phase <- c(rep(1,40), rep(2,120), rep(3,20), rep(4,40))
-    }
-    
-    if (is.data.frame(output)) {
-      output <- rbind(output, pp_df)
-    } else {
-      output <- pp_df
+      df <- g_df
     }
     
   }
   
-  # return data frame with phase added:
-  return(output)
+  if (!is.null(exp)) {
+    cat(sprintf('\n===== Experiment: %d =====\n',exp ))
+    x = df$reachdeviation_deg[which(df$condition == 'abrupt' & df$block == 'rotated')]
+    y = df$reachdeviation_deg[which(df$condition == 'gradual' & df$block == 'rotated')]
+    print(summary(BayesFactor::ttestBF(x=x, y=y, paired=TRUE)))
+    x = df$reachdeviation_deg[which(df$condition == 'abrupt' & df$block == 'clamped')]
+    y = df$reachdeviation_deg[which(df$condition == 'gradual' & df$block == 'clamped')]
+    print(summary(BayesFactor::ttestBF(x=x, y=y, paired=TRUE)))
   
-}
-
-
-# statistical functions -----
-
-getConfidenceInterval <- function(data, variance = var(data, na.rm=TRUE), conf.level = 0.95, method='t-distr', resamples=1000, FUN=mean) {
-  
-  data <- data[which(!is.na(data))]
-  
-  if (method %in% c('t-distr','t')) {
-    
-    z = qt((1 - conf.level)/2, df = length(data) - 1, lower.tail = FALSE)
-    
-    xbar = mean(data)
-    sdx = sqrt(variance/length(data))
-    
-    return(c(xbar - z * sdx, xbar + z * sdx))
-    
-  }
-  
-  # add sample z-distribution?
-  
-  if (method %in% c('bootstrap','b')) {
-    
-    data <- data[which(is.finite(data))] #need is.finite due to NA values
-    
-    samplematrix <- matrix(sample(data, size = resamples*length(data), replace = TRUE), nrow = resamples)
-    BS <- apply(samplematrix, c(1), FUN=FUN) 
-    
-    lo <- (1-conf.level)/2.
-    hi <- 1 - lo
-    
-    return(quantile(BS, probs = c(lo,hi)))
-    
+  } else {
+    # print the group-based ANOVAs:
+    for (group in names(group_BFs)) {
+      cat(sprintf('\n===== %s =====\n',toupper(group) ))
+      print(summary(group_BFs[[group]][['rotated']]))
+      print(summary(group_BFs[[group]][['clamped']]))
+    }
   }
   
 }
